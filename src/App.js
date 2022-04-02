f009f45d28720d3061f0041462089ef3519b9504
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-02 22:31:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoadingBar from 'react-top-loading-bar'
 
 const App = ()=> {
   const pageSize = 6;
-  const apiKey = "4a09c82c71d14bd4971d6a6a83ff9e4c"
+  const apiKey = process.env.REACT_APP_NEWS_API
   const [progress, setProgress] = useState(0)
  
     return (
