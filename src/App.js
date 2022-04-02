c45eecfbb6756f69fe536dda2fd6e5feb282e829
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-03 01:11:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -15,11 +15,7 @@ const App = ()=> {
       <div>
         <Router>
         <NavBar/> 
-        <LoadingBar
-        height={3}
-        color='#f11946'
-        progress={progress} 
-      />
+        <LoadingBar height={3} color='#f11946' progress={progress} />
         <Switch>
           <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
           <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/></Route> 
