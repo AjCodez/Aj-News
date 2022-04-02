2bfa03b9d3aa85ee9b3eede9053308111eef7244
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-02 22:34:44 +0530

    updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -19,7 +19,7 @@ const NewsItem = (props)=> {
                     </a>
                     <div className="card-body">
                         <a rel="noreferrer" className="card-title fs-5 fw-bold text-reset text-decoration-none text" href={newsUrl} target="_blank">{title}  </a>
-                        <p className="card-text">{description}... <a rel="noreferrer" href={newsUrl} target="_blank" className="text-reset">Read More</a></p>
+                        <p className="card-text">{description} <a rel="noreferrer" href={newsUrl} target="_blank" className="text-reset">Read More</a></p>
                         <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
                     </div>
                 </div>
