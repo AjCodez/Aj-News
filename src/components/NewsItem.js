6b9b3dfe147e87cdc5bfeb006aa3d4b981f3a330
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-04-02 17:44:14 +0530

    made changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -14,12 +14,13 @@ const NewsItem = (props)=> {
                     }> 
                         <span className="badge rounded-pill bg-danger"> {source} </span>
                     </div>
-                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <a href={newsUrl} target="_blank" rel="noreferrer">
+                        <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    </a>
                     <div className="card-body">
                         <a rel="noreferrer" className="card-title fs-5 fw-bold text-reset text-decoration-none text" href={newsUrl} target="_blank">{title}  </a>
-                        <p className="card-text">{description}</p>
+                        <p className="card-text">{description}... <a rel="noreferrer" href={newsUrl} target="_blank" className="text-reset">Read More</a></p>
                         <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
